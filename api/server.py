440a4d281c8947de717c426c4d6b67d76e11a445
choko510 <135209805+choko510@users.noreply.github.com>
2024-03-23 00:52:28 +0900

    main upload

---
+++ api/server.py
@@ -0,0 +1,216 @@
+from sqlalchemy.schema import Column
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker, declarative_base
+from fastapi import FastAPI, HTTPException, WebSocket
+from pydantic import BaseModel
+from typing import List
+import datetime
+from fastapi.middleware.cors import CORSMiddleware
+import json
+
+Base = declarative_base()
+db_file = 'count.db'
+engine = create_engine(f'sqlite:///{db_file}')
+
+class Ticket(Base):
+    __tablename__ = 'user'
+    userid = Column(String, primary_key=True)
+    password = Column(String)
+    correct = Column(Integer)
+    bad = Column(Integer)
+    correctdata = Column(String)
+    baddata = Column(String)
+
+Base.metadata.create_all(engine)
+
+class DB:
+    async def password(id):
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        user = session.query(Ticket).filter_by(userid=id).first()
+        session.close()
+        return user.password
+
+    async def registration(id, password):
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        user = Ticket(userid=id, correct=0, bad=0, password=password, correctdata="{}", baddata="{}")
+        session.add(user)
+        session.commit()
+        session.close()
+
+    async def get_correct(id):
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        user = session.query(Ticket).filter_by(userid=id).first()
+        session.close()
+        return user.correct
+
+    async def add_correct(id):
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        user = session.query(Ticket).filter_by(userid=id).first()
+        user.correct += 1
+        nowtime = str(datetime.datetime.now().year) + str(datetime.datetime.now().month) + str(datetime.datetime.now().day)
+        correctdata = json.loads(user.correctdata)
+        baddata = json.loads(user.baddata)
+
+        if correctdata is None:
+            correctdata = {}
+        if nowtime not in correctdata:
+            correctdata[nowtime] = 1
+        else:
+            correctdata[nowtime] += 1
+        if baddata is None:
+            baddata = {}
+        if nowtime not in baddata:
+            baddata[nowtime] = 0
+        
+        user.correctdata = json.dumps(correctdata)
+        user.baddata = json.dumps(baddata)
+
+        session.commit()
+        session.close()
+
+    async def add_bad(id):
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        user = session.query(Ticket).filter_by(userid=id).first()
+        user.bad += 1
+        nowtime = str(datetime.datetime.now().year) + str(datetime.datetime.now().month) + str(datetime.datetime.now().day)
+        correctdata = json.loads(user.correctdata)
+        baddata = json.loads(user.baddata)
+
+        if correctdata is None:
+            correctdata = {}
+        if nowtime not in correctdata:
+            correctdata[nowtime] = 0
+        if baddata is None:
+            baddata = {}
+        if nowtime not in baddata:
+            baddata[nowtime] = 1
+        else:
+            baddata[nowtime] += 1
+
+        user.correctdata = json.dumps(correctdata)
+        user.baddata = json.dumps(baddata)
+
+        session.commit()
+        session.close()
+
+
+    async def get_bad(id):
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        user = session.query(Ticket).filter_by(userid=id).first()
+        session.close()
+        return user.bad
+
+    async def get_all(id):
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        user = session.query(Ticket).filter_by(userid=id).first()
+        session.close()
+        return {"correct": user.correctdata, "bad": user.baddata}
+    
+    async def get(id):
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        user = session.query(Ticket).filter_by(userid=id).first()
+        session.close()
+        return {"correct": user.correct, "bad": user.bad}
+    
+
+
+fastapi = FastAPI()
+
+fastapi.add_middleware(
+            CORSMiddleware,
+            allow_origins=["*"],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+)
+
+class Data(BaseModel):
+    id: str
+    password: str
+
+@fastapi.post("/registration")
+async def registration(data: Data):
+    await DB.registration(data.id, data.password)
+    return {"message": "registration"}
+
+@fastapi.post("/get_correct")
+async def get_correct(data: Data):
+    if not await DB.password(data.id) == data.password:
+        return {"message": "password is wrong"}
+    return {"correct": await DB.get_correct(data.id)}
+
+@fastapi.post("/add_correct")
+async def add_correct(data: Data):
+    if not await DB.password(data.id) == data.password:
+        return {"message": "password is wrong"}
+    await DB.add_correct(data.id)
+    return {"message": "add_correct"}
+
+@fastapi.post("/add_bad")
+async def add_bad(data: Data):
+    if not await DB.password(data.id) == data.password:
+        return {"message": "password is wrong"}
+    await DB.add_bad(data.id)
+    return {"message": "add_bad"}
+
+@fastapi.post("/get_bad")
+async def get_bad(data: Data):
+    if not await DB.password(data.id) == data.password:
+        return {"message": "password is wrong"}
+    return {"bad": await DB.get_bad(data.id)}
+
+@fastapi.post("/get")
+async def get_bad(data: Data):
+    if not await DB.password(data.id) == data.password:
+        return {"message": "password is wrong"}
+    return await DB.get(data.id)
+
+@fastapi.get("/get/{id}/{password}")
+async def get_all(id: str, password: str):
+    if not await DB.password(id) == password:
+        return {"message": "password is wrong"}
+    return await DB.get_all(id)
+
+@fastapi.get("/ranking")
+async def ranking():
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    users = session.query(Ticket).order_by(Ticket.correct.desc()).limit(5).all()
+    session.close()
+    return [{"userid": user.userid, "correct": user.correct, "bad": user.bad} for user in users if user.correct != 0]
+
+@fastapi.post("/change/name/{newname}")
+async def change_name(data: Data, newname: str):
+    if not await DB.password(data.id) == data.password:
+        return {"message": "password is wrong"}
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    user = session.query(Ticket).filter_by(userid=data.id).first()
+    user.userid = newname
+    session.commit()
+    session.close()
+    return {"message": "change name"}
+
+@fastapi.post("/change/password/{newpassword}")
+async def change_password(data: Data, newpassword: str):
+    if not await DB.password(data.id) == data.password:
+        return {"message": "password is wrong"}
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    user = session.query(Ticket).filter_by(userid=data.id).first()
+    user.password = newpassword
+    session.commit()
+    session.close()
+    return {"message": "change password"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(fastapi, host="localhost", port=8000)
---
+++ app/index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/earlyaccess/notosansjapanese.css">
+    <link rel="stylesheet" href="style.css">
+    <title>notlet</title>
+    <link rel="manifest" href="manifest.json">
+</head>
+<body>
+    <div class="top">
+        <a href="javascript:iconmodal();"><img class="icon" src="usericon.png"></a>
+
+    </div>
+    <div class="main">
+        <div class="box1">
+            <div class="category">
+                <p>1級全般</p>
+                <a href="play/">プログラミング・ビジネス 1級</a>
+                <a href="beta/">プログラミング・ビジネス 1級 ハードモード</a>
+                <a href="play/?id=for">プログラミング・ビジネス フォール系 特訓 </a>
+            </div>
+            <div class="category">
+                <p>1級プログラミング</p>
+                <a href="play/?id=prog">プログラミング 1級</a>
+                <a href="beta/?id=proghard">プログラミング 1級 ハードモード</a>
+                <a href="play/?id=re">プログラミング リ系 特訓 </a>
+        
+            </div>
+            <div class="category">
+                <p>1級ビジネス</p>
+                <a href="play/?id=bizinesu">ビジネス 1級</a>
+                <a href="beta/?id=hardbizinesu">ビジネス 1級 ハードモード</a>
+                <a href="play/?id=excelmondai">ビジネス エクセル 1級</a>
+            </div>
+            <div class="category">
+                <p>2級全般</p>
+                <a href="play/?id=detabase">データベース全般</a>
+                <a href="play/?id=excel2">エクセル関数全般</a>
+                <a href="play/?id=mail">メールプロトコル</a>
+            </div>
+        </div>
+    </div>
+    <link rel="stylesheet" href="modal.css">
+    <script src="https://unpkg.com/micromodal/dist/micromodal.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/js-cookie@3.0.5/dist/js.cookie.min.js"></script>
+    <?php
+    if(!isset($_COOKIE['id'])){
+        $cookie_id = uniqid();
+        setcookie('id', $cookie_id, time() + 60 * 60 * 24 * 30);
+        $password = uniqid();
+        setcookie('password', $password, time() + 60 * 60 * 24 * 30);
+        echo "
+        <script>
+        async function init(id, password) {
+            const response = await fetch(`http://localhost:8000/registration`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    id: id,
+                    password: password
+                })
+            });
+        }
+        init('{$cookie_id}', '{$password}');
+        </script>
+        ";
+    }
+    ?>
+    <script>
+
+        async function fetchScores(id, password) {
+            const response = await fetch(`http://localhost:8000/get`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    id: id,
+                    password: password
+                })
+            });
+            const data = await response.json();
+            if (data.message === "password is wrong") {
+                alert("Invalid password");
+                return;
+            }
+            return data;
+        }
+
+        async function fetchRanking() {
+            const response = await fetch(`http://localhost:8000/ranking`);
+            //{"userid": userid, "correct": correct, "bad": bad}
+            const data = await response.json();
+            const ranking = data.map((d, i) => {
+                return `${i + 1}位: ${d.userid} 正解数:${d.correct} 不正解数:${d.bad}`;
+            });
+            document.getElementById("ranking").textContent = ranking;
+        }
+
+        function displayScores(correct, bad, total,ritu) {
+            document.getElementById("correct").textContent = "正解数:"+correct;
+            document.getElementById("bad").textContent = "不正解数:"+bad;
+            document.getElementById("total").textContent = "トータル:"+total;
+            document.getElementById("ritu").textContent = "正答率:"+ritu;
+        }
+        
+        
+
+        async function iconmodal() {
+            id = Cookies.get('id')
+            password = Cookies.get('password')
+            const scores = await fetchScores(id, password);
+            if (!scores) return;
+            const total = scores.correct + scores.bad;
+            const ritu = Math.round(scores.correct / total * 100) + "%";
+            MicroModal.show('modal-1');
+            displayScores(scores.correct, scores.bad,total,ritu);
+            await fetchRanking();
+        }
+    </script>
+    <div class="modal micromodal-slide" id="modal-1" aria-hidden="true">
+        <div class="modal__overlay" tabindex="-1" data-micromodal-close>
+        <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="modal-1-title" style="text-align: center;">
+            <main class="modal__content" id="modal-1-content">
+                <div>
+                    <div>
+                        <h1>マイスコア</h1>
+                        <p id="total">
+                            トータル:0
+                        </p>
+                        <p id="correct">
+                            正解数:0
+                        </p>
+                        <p id="bad">
+                            不正解数:0
+                        </p>
+                        <p id="ritu">
+                            正答率:0%
+                        </p>
+                    </div>
+                    <div>
+                        <h1>ランキング</h1>
+                        <p id="ranking"></p>
+                    </div>
+                </div>
+            </main>
+            <footer class="modal__footer">
+                <button class="modal__btn" data-micromodal-close aria-label="Close this dialog window">閉じる</button>
+            </footer>
+        </div>
+        </div>
+    </div>
+    <script>
+        
+    </script>
+</body>
+</html>
